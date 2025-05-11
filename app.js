fe798bf900bdac9b8e44d75e98fff68e4036c99c
Yuichi12231 <34160631+Yuichi12231@users.noreply.github.com>
2025-05-11 23:06:59 +0600

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ window.addEventListener("load", () => {
   let signer;
   let provider;
 
-  // ✅ Используем window.Magic для UMD-версии
-  const magic = new window.Magic("pk_live_CBB4E24015C02A64", {
+  // ✅ Используем window.MagicSDK.Magic для совместимости с UMD
+  const magic = new window.MagicSDK.Magic("pk_live_CBB4E24015C02A64", {
     oauthOptions: { providers: ["twitter"] }
   });
 
