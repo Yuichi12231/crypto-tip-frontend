bc49e896725db4497d38e28e7cd6edd58167fed1
Yuichi12231 <34160631+Yuichi12231@users.noreply.github.com>
2025-05-12 21:25:40 +0600

    Delete src directory

--- src/magic.js
+++
@@ -1,7 +0,0 @@
-
-import { Magic } from 'magic-sdk';
-import { OAuthExtension } from '@magic-ext/oauth';
-
-export const magic = new Magic('pk_live_CBB4E24015C02A64', {
-  extensions: [new OAuthExtension()],
-});
